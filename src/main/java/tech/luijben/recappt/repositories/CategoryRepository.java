e84519ec8974c3841031855b0a5226c367a4bacb
Lauwi Meara <laurasusanne@live.nl>
2023-03-11 09:31:50 +0100

    Remove unnecessary new line

--- src/main/java/tech/luijben/recappt/repositories/CategoryRepository.java
+++ src/main/java/tech/luijben/recappt/repositories/CategoryRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import tech.luijben.recappt.models.Category;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
-
     Iterable<Category> findAllByOrderByNumberAsc();
 }
